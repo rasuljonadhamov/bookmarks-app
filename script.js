dbf14b0be5e6088a279675d48097f33d9f956b47
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-09-26 15:08:07 +0300

    form validation and store bookmarks

--- script.js
+++ script.js
@@ -20,3 +20,36 @@ modalClose.addEventListener("click", () =>
 window.addEventListener("click", (e) =>
   e.target === modal ? modal.classList.remove("show-modal") : false
 );
+
+// validate form
+function validate(nameValue, urlValue) {
+  const expression =
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+  if (!nameValue || !urlValue) {
+    alert("Pleace submit values for both fields.");
+    return false;
+  }
+
+  if (!urlValue.match(regex)) {
+    alert("Pleace provide a valid web adress");
+    return false;
+  }
+
+  return true;
+}
+
+// Store bookmark
+function storeBookmark(ev) {
+  ev.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes("http://", "https://")) {
+    urlValue += `https://${urlValue}`;
+  }
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+}
+
+bookmarkForm.addEventListener("submit", storeBookmark);
