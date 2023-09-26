8cfa9d3262c48733aa59792a53fd6eab0b6de343
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-09-26 13:17:28 +0300

    add functionality to show and close modal

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const bookmarkForm = document.getElementById("bookmark-form");
 const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
 const bookmarksContainer = document.getElementById("bookmarks-container");
+
+// Show moal
+function showModal() {
+  modal.classList.add("show-modal");
+}
+
+// Add event listeners
+modalShow.addEventListener("click", showModal);
+modalClose.addEventListener("click", () =>
+  modal.classList.remove("show-modal")
+);
+
+window.addEventListener("click", (e) =>
+  e.target === modal ? modal.classList.remove("show-modal") : false
+);
